22be288e204cf086bbacae59b36d44c5d22b0b5d
Joseph Austin <joesaustin@gmail.com>
2017-07-11 00:06:22 -0400

    removed unnecessary line

--- makeArrayConsecutive2/makeArrayConsecutive.py
+++ makeArrayConsecutive2/makeArrayConsecutive.py
@@ -1,5 +1,4 @@
 def makeArrayConsecutive2(statues):
     statues.sort()
     r = range(statues[0], (statues[-1]+1))
-    l = [i for i in r if i not in statues]
-    return len(l)
+    return len([i for i in r if i not in statues])
